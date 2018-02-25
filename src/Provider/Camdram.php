84bfd6bc55778f2f09d7238b43299055e0f8c096
Peter Hoyes <pahoyes@gmail.com>
2018-02-25 03:54:38 +0000

    Oauth2 bundle for Camdram - initial commit

---
+++ src/Provider/Camdram.php
@@ -0,0 +1,116 @@
+<?php
+namespace Acts\Camdram\Oauth2\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+use Acts\Camdram\OAuth2\Provider\CamdramUser;
+use Acts\Camdram\OAuth2\Provider\Exception\CamdramIdentityProviderException;
+
+class Camdram extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+    /**
+     * Camdram domain
+     *
+     * @var string
+     */
+    public $domain = 'https://www.camdram.net';
+    
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->domain.'/oauth/v2/auth';
+    }
+    
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param  array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->domain.'/oauth/v2/token';
+    }
+    
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return $this->domain.'/auth/account.json';
+    }
+    
+    public function getAuthenticatedData($uri, AccessToken $token)
+    {
+        $options = ['headers' => 'Accept: application/json'];
+        $request = $this->getAuthenticatedRequest('GET', $this->domain.$uri, $token, $options);
+        $response = $this->getResponse($request);
+        $data = $this->parseResponse($response);
+        $this->checkResponse($response, $data);
+        return $data;
+    }
+    
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+    
+    /**
+     * Check a provider response for errors.
+     *
+     * @throws CamdramIdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  array $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw CamdramIdentityProviderException::clientException($response, $data);
+        } elseif (isset($data['error'])) {
+            throw CamdramIdentityProviderException::oauthException($response, $data);
+        }
+    }
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return \League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new CamdramUser($response);
+    }
+    
+    /**
+     * Returns the string that should be used to separate scopes when building
+     * the URL for requesting an access token.
+     *
+     * @return string Scope separator
+     */
+    protected function getScopeSeparator()
+    {
+        return ' ';
+    }
+}
---
+++ src/Provider/CamdramUser.php
@@ -0,0 +1,57 @@
+<?php
+namespace Acts\Camdram\Oauth2\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class CamdramUser implements ResourceOwnerInterface
+{
+    use ArrayAccessorTrait;
+
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
+
+    /**
+     * Creates new resource owner.
+     *
+     * @param array  $response
+     */
+    public function __construct(array $response = array())
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get resource owner id
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->getValueByKey($this->response, 'id');
+    }
+
+    /**
+     * Get resource owner name
+     *
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->getValueByKey($this->response, 'name');
+    }
+    
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+}
